1c344729887e4d4c2a385c455633d6177dcab4c5
Silvacus <49201311+Silvacus@users.noreply.github.com>
2020-10-02 00:05:41 +0530

    [Kevin]added-EmployeeAttendance

--- capgemini/EmpWageBuilder.java
+++ capgemini/EmpWageBuilder.java
@@ -2,6 +2,14 @@ package capgemini;
 
 public class EmpWageBuilder {
 	public static void main(String[] args) {
-	System.out.println("Welcome to Employee Wage Computation Problem");
-	}
-}
+		int IS_FULL_TIME=1;
+		double empcheck= Math.floor(Math.random()*10)%2; 
+		if(empcheck==IS_FULL_TIME) {
+			System.out.println("Employee present");
+			}
+		else {
+			System.out.println("Employee absent");
+		    }
+			}
+		}
+	
