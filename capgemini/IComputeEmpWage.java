595a99d94181b8a874cc1f5c1dee217c53baf7e5
Silvacus <49201311+Silvacus@users.noreply.github.com>
2020-10-02 11:35:30 +0530

    Delete IComputeEmpWage.java

--- capgemini/IComputeEmpWage.java
+++
@@ -1,7 +0,0 @@
-package capgemini;
-
-public interface IComputeEmpWage {
-	public void addCompanyEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth);
-    public void computeEmpWage();
-    public int getTotalWage(String company);
-}
